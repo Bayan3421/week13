f722561d78eb41a56b89d22a7f32a319e47e4185
Bayan3421 <115390814+Bayan3421@users.noreply.github.com>
2024-12-02 23:01:09 +0500

    Update script.py

--- script.py
+++ script.py
@@ -6,20 +6,19 @@ from streamlit_folium import st_folium
 
 st.title("Карта с маркерами КазАту")
 
-# Загрузка данных
+
 with open("coordinates.json", "r") as file:   
     locations = json.load(file)
 
-# Создаём словарь для поиска маркеров
+
 marker_dict = {loc["name"]: [loc["latitude"], loc["longitude"]] for loc in locations}
 
-# Базовая карта
+
 m = folium.Map(location=[51.187, 71.410], zoom_start=17)
 
-# Кластер маркеров
 marker_cluster = MarkerCluster()
 
-# Добавляем маркеры на карту
+
 for loc in locations:
     marker = folium.Marker(
         location=[loc["latitude"], loc["longitude"]],
@@ -30,7 +29,6 @@ for loc in locations:
 
 m.add_child(marker_cluster)
 
-# Добавляем функционал поиска
 search = Search(
     layer=marker_cluster,
     search_label="name",
@@ -39,11 +37,11 @@ search = Search(
 )
 m.add_child(search)
 
-# Боковая панель для выбора маркера
+
 st.sidebar.title("Поиск маркера")
 marker_name = st.sidebar.selectbox("Выберите маркер:", [""] + list(marker_dict.keys()))
 
-# Обновляем карту при выборе маркера
+
 if marker_name:
     coordinates = marker_dict[marker_name]
     folium.Marker(
@@ -52,7 +50,7 @@ if marker_name:
         tooltip="Выбранный маркер",
         icon=folium.Icon(color="red")
     ).add_to(m)
-    m.location = coordinates  # Перемещаем карту на выбранный маркер
+    m.location = coordinates  
     m.zoom_start = 18
 
 # Отображаем карту
